401c3459bb08f20557086c3a86c7280cea116d7f
Wilver Fabian Rojas Del Ducca <wilverfabian.06@gmail.com>
2021-10-15 15:59:26 -0500

    Home. appbar, tus cuentas

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,6 +8,7 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     final _size = MediaQuery.of(context).size;
     return Scaffold(
+      backgroundColor: const Color(0xFFE5F0F6),
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.transparent,
@@ -33,28 +34,28 @@ class Home extends StatelessWidget {
       extendBodyBehindAppBar: true,
       body: Column(
         children: [
-          Container(
-            height: _size.height * 0.5,
+          SizedBox(
+            height: _size.height * 0.42,
             child: Stack(
               children: [
                 Container(
-                  height: _size.height * 0.4,
+                  height: _size.height * 0.30,
                   decoration: const BoxDecoration(
                     color: Color(0xFF004481),
                   ),
                 ),
                 Positioned(
                   bottom: 0,
-                  left: 0,
-                  right: 0,
+                  left: 5,
+                  right: 5,
                   child: Card(
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(10.0),
                     ),
-                    elevation: 10,
+                    elevation: 2,
                     child: Padding(
                       padding: const EdgeInsets.symmetric(
-                          horizontal: 10, vertical: 10),
+                          horizontal: 20, vertical: 20),
                       child: Column(
                         children: [
                           Row(
@@ -64,10 +65,104 @@ class Home extends StatelessWidget {
                                 'TUS CUENTAS',
                                 style: TextStyle(
                                   fontSize: 15,
+                                  color: Color(0xFF004481),
+                                  fontWeight: FontWeight.bold,
                                 ),
                               ),
                               SvgPicture.asset('assets/images/punticos.svg'),
                             ],
+                          ),
+                          const SizedBox(height: 10),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: const [
+                                    Text(
+                                      '001ah7297',
+                                      style: TextStyle(
+                                        color: Color(0xFF1873B9),
+                                        fontSize: 17,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                    Text(
+                                      '*37265',
+                                      style: TextStyle(
+                                        color: Color(0xFF839BB1),
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                const Spacer(),
+                                const Text(
+                                  '\$20,0000',
+                                  style: TextStyle(
+                                    color: Color(0xFF004481),
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                const SizedBox(width: 30),
+                                const Icon(
+                                  Icons.keyboard_arrow_right,
+                                  color: Color(0xFF004380),
+                                ),
+                              ],
+                            ),
+                          ),
+                          const Divider(
+                            endIndent: 0.5,
+                            thickness: 0.1,
+                            color: Colors.black,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: const [
+                                    Text(
+                                      '001ah7297',
+                                      style: TextStyle(
+                                        color: Color(0xFF1873B9),
+                                        fontSize: 17,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                    Text(
+                                      '*37265',
+                                      style: TextStyle(
+                                        color: Color(0xFF839BB1),
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                const Spacer(),
+                                const Text(
+                                  '\$20,0000',
+                                  style: TextStyle(
+                                    color: Color(0xFF004481),
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                const SizedBox(width: 30),
+                                const Icon(
+                                  Icons.keyboard_arrow_right,
+                                  color: Color(0xFF004380),
+                                ),
+                              ],
+                            ),
                           ),
                         ],
                       ),
@@ -77,9 +172,81 @@ class Home extends StatelessWidget {
               ],
             ),
           ),
-          Center(
-            child: Text('Hola Mundo'),
+          const SizedBox(height: 10),
+          Container(
+            color: Colors.white,
+            width: _size.width,
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 15.0, horizontal: 20),
+              child: Row(
+                children: [
+                  Column(
+                    children: [
+                      Container(
+                        decoration: const BoxDecoration(
+                          color: Colors.orange,
+                          shape: BoxShape.circle,
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.all(20.0),
+                          child: Icon(
+                            Icons.add,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      const Text(
+                        'Oportunidades',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 11,
+                          fontWeight: FontWeight.w600,
+                          color: Color(0xFF4B637A),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(width: 20),
+                  Column(
+                    children: [
+                      Container(
+                        decoration: const BoxDecoration(
+                          color: Colors.orange,
+                          shape: BoxShape.circle,
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.all(20.0),
+                          child: Icon(
+                            Icons.add,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      const Text(
+                        'Transferir',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 11,
+                          fontWeight: FontWeight.w600,
+                          color: Color(0xFF4B637A),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ),
+          // const SingleChildScrollView(
+          //   scrollDirection: Axis.horizontal,
+          //   child: SizedBox(
+          //     height: 200,
+          //     child: ,
+          //   ),
+          // ),
         ],
       ),
     );
